28a06a874a8ee850dd07901c7816b0a334ea26b4
Tony Knight <tonycknight@users.noreply.github.com>
2026-01-19 21:46:46 +0000

    first basic acceptance test

--- tests/microbroker.tests.acceptance/Tests.fs
+++ tests/microbroker.tests.acceptance/Tests.fs
@@ -4,5 +4,29 @@ open System
 open Xunit
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Get Heartbeat`` () =
+    task {
+        use client = new System.Net.Http.HttpClient()
+        
+        let uri = "http://localhost:8080/heartbeat/"
+        let! r = client.GetAsync(uri)
+
+        let _ = r.EnsureSuccessStatusCode()
+
+        return true
+
+    }
+
+[<Fact>]
+let ``Get Queues`` () =
+    task {
+        use client = new System.Net.Http.HttpClient()
+        
+        let uri = "http://localhost:8080/queues/"
+        let! r = client.GetAsync(uri)
+
+        let _ = r.EnsureSuccessStatusCode()
+
+        return true
+
+    }
