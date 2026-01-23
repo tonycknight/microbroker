23a0d2f16078c5ffb81bd652b6f5ef37390d9964
Tony Knight <tonycknight@users.noreply.github.com>
2026-01-23 23:10:43 +0000

    refactor

--- tests/microbroker.tests.performance/Program.fs
+++ tests/microbroker.tests.performance/Program.fs
@@ -13,73 +13,69 @@ module Program =
     let messageUrl = "http://localhost:8080/queues/perftests/message/"
 
     let setSimulation = 
-        Scenario.withWarmUpDuration (seconds 10)
-        >> Scenario.withLoadSimulations [ Inject(rate = 1000, interval = seconds 1, during = seconds 30) ] 
+        Scenario.withWarmUpDuration (seconds 15)
+        >> Scenario.withLoadSimulations [ Inject(rate = 100, interval = seconds 1, during = seconds 30) ] 
 
+    let getQueues httpClient context =
+        task {
+                    
+            let request = Http.createRequest "GET" queuesUrl
+                    
+            let! response = request |> Http.send httpClient
 
+            return response
+        }
+
+    let pushMessage httpClient context =
+        task {
+            let now = DateTimeOffset.UtcNow
+
+            let msg = { QueueMessage.content = $"Message {Guid.NewGuid()}"
+                        messageType = "text/plain" 
+                        active = now
+                        created = now
+                        expiry = now.AddMinutes(10.0) }
+
+            let request = 
+                Http.createRequest "POST" messageUrl
+                |> Http.withJsonBody msg
+                                    
+            let! response = request |> Http.send httpClient
+
+            return response                                
+        }
+
+    let pullMessage httpClient context = 
+        task {
+            let request = Http.createRequest "GET" messageUrl
+                
+            let! response = request |> Http.send httpClient
+
+            return response
+        }
+
+    let pushPullMessage httpClient context =
+        task {
+
+            let! post = Step.run("post message", context, (pushMessage httpClient))
+
+            if post.IsError then
+                return Response.fail ("", "Unexpected status code")
+            else
+                let! get = Step.run("get message", context, (pullMessage httpClient))
+                return Response.ok()
+        }
 
     [<EntryPoint>]
     let main argv =
         
         use httpClient = Http.createDefaultClient()
-
-        let getQueues context =
-            task {
-                    
-                let request = Http.createRequest "GET" queuesUrl
-                    
-                let! response = request |> Http.send httpClient
-
-                return match response.IsError with
-                        | false ->   Response.ok()
-                        | _ ->      Response.fail (response.StatusCode, "Unexpected status code")                                                
-            }
-
-        let pushMessage context =
-            task {
-                let now = DateTimeOffset.UtcNow
-
-                let msg = { QueueMessage.content = $"Message {Guid.NewGuid()}"
-                            messageType = "text/plain" 
-                            active = now
-                            created = now
-                            expiry = now.AddMinutes(10.0) }
-
-                let request = 
-                    Http.createRequest "POST" messageUrl
-                    |> Http.withJsonBody msg
-                                    
-                let! response = request |> Http.send httpClient
-
-                return response                                
-            }
-
-        let pullMessage context = 
-            task {
-                let request = Http.createRequest "GET" messageUrl
-                
-                let! response = request |> Http.send httpClient
-
-                return response
-            }
-
-        let pushPullMessage context =
-            task {
-
-                let! post = Step.run("post message", context, pushMessage)
-
-                if post.IsError then
-                    return Response.fail ("", "Unexpected status code")
-                else
-                    let! get = Step.run("get message", context, pullMessage)                    
-                    return Response.ok()
-            }
         
         let pushPull = 
-            Scenario.create ("push pull message", pushPullMessage) |> setSimulation
+            Scenario.create ("push pull message", pushPullMessage httpClient) |> setSimulation
                 
         let getCounts =
-            Scenario.create ("get queues", getQueues) |> setSimulation
+            Scenario.create ("get queues", getQueues httpClient) |> setSimulation
 
         NBomberRunner.registerScenarios [ pushPull; getCounts ]
         |> NBomberRunner.run |> ignore
