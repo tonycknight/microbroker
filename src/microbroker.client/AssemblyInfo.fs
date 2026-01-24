b11cf4b17b50022335a0bb957b925c157f8ab4f6
Tony Knight <tonycknight@users.noreply.github.com>
2026-01-24 01:19:14 +0000

    add client lib

---
+++ src/microbroker.client/AssemblyInfo.fs
@@ -0,0 +1,8 @@
+﻿module AssemblyInfo
+
+open System.Runtime.CompilerServices
+
+[<assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")>]
+[<assembly: InternalsVisibleTo("Microbroker.Client.Tests")>]
+[<assembly: InternalsVisibleTo("Microbroker.Client.Tests.Integration")>]
+do ()
---
+++ src/microbroker.client/MicrobrokerMessage.fs
@@ -0,0 +1,46 @@
+﻿namespace Microbroker.Client
+
+open System
+
+[<CLIMutable>]
+type MicrobrokerMessage =
+    { messageType: string
+      content: string
+      created: DateTimeOffset
+      active: DateTimeOffset
+      expiry: DateTimeOffset }
+
+module MicrobrokerMessages =
+    let internal fromString (value: string) =
+        try
+            Newtonsoft.Json.JsonConvert.DeserializeObject<MicrobrokerMessage>(value) |> Some
+        with ex ->
+            None
+
+    let internal toJsonArray (messages: MicrobrokerMessage[]) =
+        Newtonsoft.Json.JsonConvert.SerializeObject(messages)
+
+    let create () =
+        let now = Time.now ()
+
+        { MicrobrokerMessage.created = now
+          content = ""
+          messageType = ""
+          active = DateTimeOffset.MinValue
+          expiry = DateTimeOffset.MaxValue }
+
+    let active (active: unit -> DateTimeOffset) (message: MicrobrokerMessage) = { message with active = active () }
+
+    let delayed (delay: unit -> TimeSpan) (message: MicrobrokerMessage) =
+        let active = Time.now () |> Time.add (delay ())
+        { message with active = active }
+
+    let messageType messageType (message: MicrobrokerMessage) =
+        { message with
+            messageType = messageType }
+
+    let content content (message: MicrobrokerMessage) = { message with content = content }
+
+    let expiry (delay: unit -> TimeSpan) (message: MicrobrokerMessage) =
+        let expires = Time.now () |> Time.add (delay ())
+        { message with expiry = expires }
