3fdc29d94beea4f79128d033f413a828267e7eb5
Tony Knight <tonycknight@users.noreply.github.com>
2026-02-01 17:27:25 +0000

    clean tests

--- tests/microbroker.client.tests.unit/ThrottlingTests.fs
+++ tests/microbroker.client.tests.unit/ThrottlingTests.fs
@@ -9,60 +9,66 @@ module ThrottlingTests =
 
     [<Fact>]
     let ``exponentialWait on success yields single iteration`` () =
-        let duration = TimeSpan.FromSeconds 1.
-        let mutable count = 0
+        task {
+            let duration = TimeSpan.FromSeconds 1.
+            let mutable count = 0
 
-        let f () =
-            task {
-                count <- count + 1
-                return Some count
-            }
+            let f () =
+                task {
+                    count <- count + 1
+                    return Some count
+                }
 
-        let r = (Throttling.exponentialWait duration f).Result
+            let! r = Throttling.exponentialWait duration f
 
-        count |> should equal 1
+            count |> should equal 1
+        }
 
     [<Fact>]
     let ``exponentialWait on None yields multiple iterations`` () =
-        let duration = TimeSpan.FromSeconds 1.
-        let mutable count = 0
-        let iterations = 3
+        task {
+            let duration = TimeSpan.FromSeconds 1.
+            let mutable count = 0
+            let iterations = 3
 
-        let f () =
-            task {
-                count <- count + 1
+            let f () =
+                task {
+                    count <- count + 1
 
-                return
-                    match count < iterations with
-                    | true -> None
-                    | _ -> Some count
-            }
+                    return
+                        match count < iterations with
+                        | true -> None
+                        | _ -> Some count
+                }
 
-        let r = (Throttling.exponentialWait duration f).Result
+            let! r = Throttling.exponentialWait duration f
 
-
-        count |> should equal iterations
+            count |> should equal iterations
+        }
 
 
     [<Fact>]
     let ``exponentialWait on exception yields multiple iterations`` () =
-        let duration = TimeSpan.FromSeconds 1.
-        let mutable count = 0
-        let iterations = 3
+        task {
+            let duration = TimeSpan.FromSeconds 1.
+            let mutable count = 0
+            let iterations = 3
 
-        let f () =
-            task {
-                count <- count + 1
+            let f () =
+                task {
+                    count <- count + 1
 
-                if count < iterations then
-                    invalidOp "broken"
+                    if count < iterations then
+                        invalidOp "broken"
 
-                return Some count
-            }
+                    return Some count
+                }
 
-        try
-            (Throttling.exponentialWait duration f).Result |> ignore
-        with ex ->
-            ignore 0
+            try
+                let! r = Throttling.exponentialWait duration f
+                ignore r
+            with ex ->
+                ignore 0
 
-        count |> should equal 1
+            count |> should equal 1
+        }
